01e40b8c14e16437b14a1c6aee529ca0d5583182
LouisMayor <louismayor1@gmail.com>
2019-05-16 22:25:10 +0100

    project commit

---
+++ Assets/Code/DeveloperConsole.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleCommand
+{
+    public string Name;
+    public string Description;
+    public Action Function;
+}
+
+[ExecuteInEditMode]
+public class DeveloperConsole : MonoBehaviour
+{
+    [SerializeField] private KeyCode m_ActivationKey = KeyCode.BackQuote;
+    [SerializeField] private KeyCode m_AcceptInput = KeyCode.KeypadEnter;
+
+    [SerializeField] private InputField m_ConsoleText;
+    [SerializeField] private Text m_InputText;
+
+    protected DeveloperConsoleUI m_ConsoleUI;
+
+    private Dictionary<string, ConsoleCommand> m_Commands;
+
+    public static DeveloperConsole Instance { get; private set; }
+    public bool IsConsoleVisible { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        if (m_Commands == null)
+        {
+            m_Commands = new Dictionary<string, ConsoleCommand>();
+        }
+
+        if (m_InputText == null)
+        {
+            m_InputText = GetComponentInChildren<Text>();
+        }
+
+        if(m_ConsoleText == null)
+        {
+            m_ConsoleText = GetComponentInChildren<InputField>();
+        }
+
+        Hide();
+    }
+
+    protected virtual void Hide()
+    {
+        m_ConsoleText.DeactivateInputField();
+
+        // Thanks Unity, make something soo bloody difficult to turn off
+        m_ConsoleText.readOnly = true;
+        m_ConsoleText.enabled = false;
+    }
+
+    protected virtual void Show()
+    {
+        m_ConsoleText.enabled = true;
+        m_ConsoleText.readOnly = false;
+        m_ConsoleText.ActivateInputField();
+        m_ConsoleText.text = string.Empty;
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(m_ActivationKey))
+        {
+            IsConsoleVisible = !IsConsoleVisible;
+
+            if(IsConsoleVisible)
+            {
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+        }
+
+        if (IsConsoleVisible)
+        {
+            if (Input.GetKeyDown(m_AcceptInput) && m_ConsoleText.textComponent.text != "")
+            {
+                SubmitCommand(m_ConsoleText.textComponent.text);
+            }
+        }
+    }
+
+    public void AddCommand(string cmdName, ConsoleCommand command)
+    {
+        m_Commands.Add(cmdName, command);
+    }
+
+    public void RemoveCommand(string cmdName)
+    {
+        m_Commands.Remove(cmdName);
+    }
+
+    private void Print(string text)
+    {
+        m_ConsoleText.text += text + "\n";
+    }
+
+    private void SubmitCommand(string cmdName)
+    {
+        if (!VerifyInput(ref cmdName))
+        {
+            Print("Invalid text");
+            return;
+        }
+
+        m_Commands.TryGetValue(cmdName, out ConsoleCommand command);
+        if (command != null)
+        {
+            command.Function.Invoke();
+        }
+        else
+        {
+            Print("Failed to find command. Type \'Help\' to find out more");
+        }
+    }
+
+
+    private bool VerifyInput(ref string cmdName)
+    {
+        return !string.IsNullOrEmpty(cmdName) && !string.IsNullOrWhiteSpace(cmdName);
+    }
+}
---
+++ Assets/Code/DeveloperConsoleUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeveloperConsoleUI : DeveloperConsole
+{
+    [SerializeField] private Canvas m_ConsoleCanvas;
+    [SerializeField] private Image m_ActiveFieldBackground;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if(m_ConsoleCanvas == null)
+        {
+            m_ConsoleCanvas = GetComponentInChildren<Canvas>();
+        }
+
+        if (m_ActiveFieldBackground == null)
+        {
+            m_ActiveFieldBackground = GetComponentInChildren<Image>();
+        }
+
+        m_ConsoleUI = this;
+    }
+
+    protected override void Hide()
+    {
+        base.Hide();
+
+        m_ConsoleCanvas.enabled = false;
+        m_ActiveFieldBackground.enabled = false;
+    }
+
+    protected override void Show()
+    {
+        m_ConsoleCanvas.enabled = true;
+        m_ActiveFieldBackground.enabled = true;
+
+        base.Show();
+    }
+}
